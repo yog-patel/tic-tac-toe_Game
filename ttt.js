787ecd75b783e896efe916f50c738b1deb28b5b5
yog-patel <122759187+yog-patel@users.noreply.github.com>
2024-08-30 02:01:55 +0530

    1.0

---
+++ ttt.js
@@ -0,0 +1,76 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnX = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+const resetGame = () => {
+    turnX = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(turnX){
+            box.innerText = "X";
+            turnX = false;
+        }else{
+            box.innerText = "O";
+            turnX = true;
+        }
+        console.log("box was clicked");
+        box.disabled = true;
+        checkForWin();
+    })
+})
+
+const disableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `${winner} Wins`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const checkForWin = () => {
+    for(let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val == pos2Val && pos2Val == pos3Val){
+                console.log("winner found");
+                showWinner(pos1Val);
+            }
+        }
+    }
+}
+
+newGameBtn.addEventListener("click", resetGame);
+
+resetBtn.addEventListener("click", resetGame);
